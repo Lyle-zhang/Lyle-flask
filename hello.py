3852a16ea6fa220701f3ca8f09dc1b882c526160
Lyle-zhang <zhanglinyao1209@gmail.com>
2016-03-09 22:15:23 +0800

    # You must commit a session to write sth to a db!

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import pdb
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
@@ -68,13 +69,26 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data)
+        # pdb.set_trace()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            # You must commit a session to write sth to a db!
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        # pdb.set_trace()
+
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
         session['name'] = form.name.data
         return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -83,5 +97,5 @@ def user(name):
 
 
 if __name__ == '__main__':
-    db.create_all()
-    manager.run()
+    # db.create_all()
+    app.run(debug=True)
